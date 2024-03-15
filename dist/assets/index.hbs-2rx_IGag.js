975fcbe5d8b3f788003e2dee813ed442c0df063c
tomvanenckevort-hp <tomvanenckevort-hp@users.noreply.github.com>
2024-03-15 12:23:27 +0000

    Automated build

--- dist/assets/index.hbs-2rx_IGag.js
+++
@@ -1,1 +0,0 @@
-import"./main-CC6vhd9u.js";const o=new URLSearchParams(window.location.search),r=o.get("organization-name")??"",e=document.querySelector("#ce-tech-suggest-correction-form");e.src=`${e.dataset.src}${r}`;
